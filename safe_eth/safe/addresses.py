702fcb4445b8440a3731132587ac5497d6f0b798
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-20 00:03:23 +0300

    Add missing addresses for ETHERLINK_TESTNET in addresses.py

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1682,12 +1682,20 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 4886937, "1.4.1+L2"),  # v1.4.1+L2
     ],
     EthereumNetwork.ETHERLINK_TESTNET: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1160050, "1.3.0"),  # v1.3.0
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1160050, "1.3.0+L2"),  # v1.3.0+L2
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 3826171, "1.3.0"),  # v1.3.0
         (
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
             3826169,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 15388040, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            15388048,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
     ],
     EthereumNetwork.KAVA_TESTNET: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 11835211, "1.3.0"),  # v1.3.0
@@ -3009,7 +3017,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 1),  # v1.4.1
     ],
     EthereumNetwork.ETHERLINK_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1160050),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 3826151),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 15387981),  # v1.4.1
     ],
     EthereumNetwork.KAVA_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 11835195),  # v1.3.0
